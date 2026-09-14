628ee192532cc3847d3c63dd4e4e1ce18d023664
paulinozenaida18-ux <paulinozenaida18@gmail.com>
2026-09-14 10:29:57 -0500

    Version APi

--- app/api.py
+++ app/api.py
@@ -3,9 +3,17 @@ import joblib
 
 app = FastAPI()
 
-# joblib.load(...) carga el modelo entrenado
+# Cargamos el modelo entrenado
 modelo = joblib.load("models/modelo.pkl")
 
 @app.get("/")
 def inicio():
     return {"estado": "activo"}
+
+@app.get("/predict")
+def predict(dia: int):
+    resultado = modelo.predict([[dia]])
+    return {
+        "dia": dia,
+        "prediccion": float(resultado[0])
+    }
