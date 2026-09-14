4eccf484764457227a3a7f329864d0a43bf73e55
paulinozenaida18-ux <paulinozenaida18@gmail.com>
2026-09-14 10:03:24 -0500

    VersionApi

--- app/api.py
+++ app/api.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI
 import joblib
 
 app = FastAPI()
+
 # joblib.load(...) carga el modelo entrenado
 modelo = joblib.load("models/modelo.pkl")
 
 @app.get("/")
 def inicio():
-return {"estado":"activo"}
+    return {"estado": "activo"}
